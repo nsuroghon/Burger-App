dac88fdbcb1bf3e5d46e94cc69bf68da29c446ef
Niv Suroghon <nsuroghon@gmail.com>
2021-01-12 16:03:03 -0500

    get rq router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,13 @@ const express = require('express');
 const burger = require('../models/burger.js')
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
