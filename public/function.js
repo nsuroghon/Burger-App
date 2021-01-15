48eafa9d786097f4b271e6625e96c520fbe37859
Niv Suroghon <nsuroghon@gmail.com>
2021-01-14 20:04:44 -0500

    front end updates

--- public/function.js
+++ public/function.js
@@ -1,37 +1,31 @@
-$(function() {
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#newburger")
-          .val()
-          .trim(),
-        devoured: 0
-      };
-  
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(function() {
-        console.log("Added new burger");
-        location.reload();
-      });
-    });
-  
-    $(".eatburger").on("click", function(event) {
-      event.preventDefault();
-  
-      var id = $(this).data("id");
-      var devouredState = {
-        devoured: 1
-      };
-  
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: devouredState
-      }).then(function() {
-        console.log("Burger devoured");
-        location.reload();
-      });
-    });
+const { response } = require("express");
+const { create } = require("express-handlebars");
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (event) {
+    console.info('DOM loaded')
+  }
+});
+
+// CREATE NEW BURGER
+const formButton = document.querySelector("#addburger");
+
+if (formButton) {
+  formButton.addEventListener("submit", function(event){
+    event.preventDefault();
+  })
+
+  var newBurger = {
+    burger_name: document.getElementById("newburger").val().trim(),
+    devoured: 0
+  };
+
+  fetch("/api/burgers", {
+    type: "POST",
+    data: newBurger
+  }).then(() => {
+      console.log('Added new burger');
+      location.reload();
   });
+};
