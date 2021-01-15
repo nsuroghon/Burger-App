402a788759faf79f6cf0e1392d83d7d6f930a132
Niv Suroghon <nsuroghon@gmail.com>
2021-01-14 21:23:27 -0500

    jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'niv',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'suroghon',
-  database: 'burgers_db',
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    })
+};
 
 // Make connection.
 connection.connect((err) => {
